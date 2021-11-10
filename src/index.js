61ae3ce5ca688a9269eb0f1a6aa5c50f4d3a0574
Lindow-N <anthonycasaresit@gmail.com>
2021-11-10 17:10:01 +0100

    fr

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
-
+ReactDOM.render((
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
+))
